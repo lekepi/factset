f66449ca8ca85f8f638dd12a4c72783b6fcddcb6
olivier <lekepi@gmail.com>
2023-04-18 15:06:02 +0100

    loop checker2

--- utils.py
+++ utils.py
@@ -59,7 +59,7 @@ def simple_email(subject, body, ml, html=None):
 def loop_checker(minutes=30):
     today = datetime.now()
     current_time = datetime.now().hour + datetime.now().minute / 60
-    if current_time > 6 and today.weekday() < 5:
+    if current_time > 7 and today.weekday() < 5:
         loop_projects = ['Factset', 'Task Extra', 'Get Trade EMSX', 'PB NAV Reporting']
         today_dt = datetime(year=today.year, month=today.month, day=today.day)
         today_str = today.strftime('%Y-%m-%d')
@@ -74,15 +74,15 @@ def loop_checker(minutes=30):
                 if log_project_list:
                     log_project = log_project_list[0]
                     # get the difference between log_project.date_time and now in minutes
-                    minute_diff = (datetime.now() - log_project.date_time).total_seconds() / 60
+                    minute_diff = int((datetime.now() - log_project.date_time).total_seconds() / 60)
                     if minute_diff > 30:
-                        subject = f'ERROR - Loop {project} not working - {today_str}'
+                        subject = f"ERROR - Loop '{project}' not working - {today_str}"
                         comment = f"'{project}' has not run for {int(minute_diff)} minutes, please contact Olivier"
                         task_checker_db('Fail', f'Loop {project} - {today_str}',
                                         comment=comment, task_name='Loop', task_type='Task Scheduler')
                         simple_email(subject, comment, config_class.MAIL_BO)
                 else:
-                    subject = f'ERROR - Loop {project} not working - {today_str}'
+                    subject = f"ERROR - Loop '{project}' not working - {today_str}"
                     comment = f"'{project}' has not run at all today, please contact Olivier"
                     task_checker_db('Fail', f'Loop {project} - {today_str}',
                                     comment=comment, task_name='Loop', task_type='Task Scheduler')
